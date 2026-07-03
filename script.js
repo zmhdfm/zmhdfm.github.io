a7c23dd07c89b69a376f33711ed993665c09a097
zmhmrqc <3603018764@qq.com>
2026-07-03 18:08:44 +0800

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const footer = document.querySelector('.footer');
-    if (footer) {
-        const currentYear = new Date().getFullYear();
-        footer.textContent = footer.textContent.replace(/© \d{4}/, `© ${currentYear}`);
-    }
-    console.log('网站加载完成！');
-});
